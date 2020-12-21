3081fe262b50b674bc5cdf94e44f020524b7879f
Vasiliy Makarov <drmoriarty.0@gmail.com>
2020-12-21 20:52:05 +0300

    set func types in wrapper

--- scripts/appsflyer.gd
+++ scripts/appsflyer.gd
@@ -28,39 +28,39 @@ func _ready():
     if _localnotification != null:
         _localnotification.connect('device_token_received', self, '_on_device_token_received')
 
-func init(app_key):
+func init(app_key: String) -> void:
     if _i != null:
         var production = !OS.is_debug_build()
         print('AppsFlyer plugin inited with key')
         _i.init(app_key, production)
 
-func init_with_id(app_key, app_id):
+func init_with_id(app_key: String, app_id: String) -> void:
     if _i != null:
         print('AppsFlyer plugin inited with key and app id')
         _i.init(app_key, app_id)
 
-func screen(name, screen_type):
+func screen(name: String, screen_type: String) -> void:
     _track_event('af_content_view', {'af_content': name, 'af_content_type': screen_type})
 
-func level_start(params):
+func level_start(params: Dictionary) -> void:
     _track_event('level_start', {'af_level': params.level, 'stage': params.stage})
 
-func level_complete(params):
+func level_complete(params: Dictionary) -> void:
     _track_event('af_level_achieved', {'af_level': params.level, 'stage': params.stage, 'stars': params.stars, 'af_score': params.score, 'time': params.time, 'af_success': true, 'continue': params.continue, 'challenge': params.challenge})
 
-func level_failed(params):
+func level_failed(params: Dictionary) -> void:
     _track_event('level_failed', {'af_level': params.level, 'stage': params.stage, 'continue': params['continue'], 'time': params.time, 'completion': params.completion})
 
-func level_really_failed(params):
+func level_really_failed(params: Dictionary) -> void:
     _track_event('level_really_failed', {'af_level': params.level, 'stage': params.stage, 'continue': params['continue'], 'time': params.time, 'completion': params.completion})
 
-func login(params):
+func login(params: Dictionary) -> void:
     _track_event('af_login', {'af_registration_method': params.method, 'af_success': params.success})
 
-func event(name, params):
+func event(name: String, params: Dictionary) -> void:
     _track_event(name, params)
 
-func appsflyer_id():
+func appsflyer_id() -> String:
     if _cached_appsflyer_id != null:
         return _cached_appsflyer_id
     if _i != null:
@@ -73,12 +73,12 @@ func appsflyer_id():
 # Internal methods
 #
 
-func _track_event(event, params = {}):
+func _track_event(event: String, params := {}) -> void:
     if params == null:
         params = {}
     if _i != null:
         _i.track_event(event, params)
 
-func _on_device_token_received(token):
+func _on_device_token_received(token: String) -> void:
     if _i != null:
         _i.set_uninstall_token(token)
